a22e342f80320549e9aceca1134eb54d45776d9d
baguswahyu <58266965+baguswahyu@users.noreply.github.com>
2020-06-06 16:42:23 +0800

    Update autoSQM.sh

--- autoSQM.sh
+++ autoSQM.sh
@@ -1,35 +1,16 @@
- /etc/init.d/sqm stop
- if [ ! -d /tmp/speedtestResult ]; then
- mkdir /tmp/speedtestResult
- fi
- sh /root/betterspeedtest.sh -t 15 -n 15 -p www.google.com -H netperf-west.bufferbloat.net >> /tmp/speedtestResult/speedtestLog-"$(date +"%Y-%m-%d").log"
- /etc/init.d/sqm start
- 
- #Get list of speedtest
- cat /tmp/speedtestResult/speedtestLog-"$(date +"%Y-%m-%d").log" | grep -i download | tr -d "Download: " | tr -d Mbps | sort -n > /tmp/download.txt
- cat /tmp/speedtestResult/speedtestLog-"$(date +"%Y-%m-%d").log" | grep -i upload | tr -d "Upload: " | tr -d Mbps | sort -n > /tmp/upload.txt
- 
- #get average speed for download & upload
- download=`cat /tmp/download.txt  | awk -f median.awk`
- upload=`cat /tmp/upload.txt  | awk -f median.awk`
- 
- #convert to Kbps & adjust to 80% speed
- downloadKbps=$(awk "BEGIN {download = $download; print download*1000*80/100}")
- uploadKbps=$(awk "BEGIN {upload = $upload; print upload*1000*80/100}")
- echo $downloadKbps
- echo $uploadKbps
- 
- #set value to SQM use uci
- #change value after -gt to only apply SQM if the result greate than X (in kbps)
- if [ "$downloadKbps" -gt 10000 ] && [ "$uploadKbps" -gt 10000 ]; then
- echo "setting up SQM with new value"
- uci set sqm.eth1.upload=${uploadKbps%%.*}
- uci set sqm.eth1.download=${downloadKbps%%.*}
- uci commit
- /etc/init.d/sqm reload
- echo "auto sqm DONE!"
- logger -t autoSQM -p info "Set download = $downloadKbps, upload = $uploadKbps"
- fi
- 
- echo "auto sqm DONE!"
- logger -t autoSQM -p info "Set download = $downloadKbps, upload = $uploadKbps"
+/etc/init.d/sqm stop
+sh monitor.sh & APP_PID=$!                              
+wget -O /dev/null http://speedtest.tele2.net/20MB.zip
+echo $APP_PID        
+kill -9 $APP_PID
+download=`cat /root/download.txt  | awk -f median.awk`
+downloadKbps=$(awk "BEGIN {download = $download; print download*90/100}")
+echo $downloadKbps
+if [ "$downloadKbps" -gt 1000 ]; then
+echo "setting up SQM with new value"
+uci set sqm.eth1.download=${downloadKbps%%.*}
+uci commit
+echo "auto sqm DONE!"
+logger -t autoSQM -p info "Set download = $downloadKbps"
+fi
+/etc/init.d/sqm start
