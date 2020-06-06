c2d5410e3151b083537750e0c7d7c626ecd6dcb2
baguswahyu <58266965+baguswahyu@users.noreply.github.com>
2020-06-06 17:11:16 +0800

    Update autoSQM.sh

--- autoSQM.sh
+++ autoSQM.sh
@@ -3,10 +3,10 @@ sh monitor.sh & APP_PID=$!
 wget -O /dev/null http://speedtest.tele2.net/20MB.zip
 echo $APP_PID        
 kill -9 $APP_PID
-download=`cat /root/download.txt  | awk -f median.awk`
+download=`sort -n /root/download.txt  | awk -f median.awk`
 downloadKbps=$(awk "BEGIN {download = $download; print download*90/100}")
 echo $downloadKbps
-if [ "$downloadKbps" -gt 1000 ]; then
+if [ "$downloadKbps" -gt 2500 ]; then
 echo "setting up SQM with new value"
 uci set sqm.eth1.download=${downloadKbps%%.*}
 uci commit
